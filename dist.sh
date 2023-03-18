98543f2fcadab425bdc267eb608e9595fd1af8be
Hunter Herman <hherman@palantir.com>
2023-03-18 16:33:43 -0700

    fix dist.sh

--- dist.sh
+++ dist.sh
@@ -5,10 +5,10 @@ set -x
 rm -rf build/*
 
 GOOS=darwin GOARCH=amd64 go build -o build/cilogs .
-zip -j build/darwin-amd64.zip build/gq
+zip -j build/darwin-amd64.zip build/cilogs
 GOOS=darwin GOARCH=arm64 go build -o build/cilogs .
-zip -j build/darwin-arm64.zip build/gq
+zip -j build/darwin-arm64.zip build/cilogs
 GOOS=linux GOARCH=amd64 go build -o build/cilogs .
-zip -j build/linux-amd64.zip build/gq
+zip -j build/linux-amd64.zip build/cilogs
 GOOS=windows GOARCH=amd64 go build -o build/cilogs .
-zip -j build/windows-amd64.zip build/gq
+zip -j build/windows-amd64.zip build/cilogs
